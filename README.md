73e2c2c59e7dfbf64b1a8a131f0d54f116afc331
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-01-05 03:48:13 +0500

    create capston project

---
+++ README.md
@@ -0,0 +1,127 @@
+<a name="readme-top"></a>
+
+<div align="center">
+  <img src="murple_logo.png" alt="logo" width="140"  height="auto" />
+  <br/>
+
+  <h3><b>Microverse README Template</b></h3>
+
+</div>
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Tech Stack](#tech-stack)
+- [👥 Authors](#authors)
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 Portfolio Project <a name="about-project"></a>
+
+> capston project built-with html css and javaScript
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+> This project is built-with **html** **css** and **javaScript**.
+
+<!-- Features -->
+
+### Key Features <a name="key-features"></a>
+
+> Describe between 1-3 key features of the application.
+
+- **[key_feature_1]**
+- **[key_feature_2]**
+- **[key_feature_3]**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🚀 Live Demo <a name="live-demo"></a>
+
+> Add a link to your deployed project.
+
+- [Live Demo Link](http://127.0.0.1:5500/index.html#)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+> Just open index.html to open the project
+
+### Prerequisites
+
+In order to run the tests you need node.js installed.
+
+### Install
+
+Install this project with:
+
+```sh
+npm i
+```
+
+### Usage
+
+Open `index.html`.
+
+### Run tests
+
+To run tests, run the following command:
+
+```sh
+npm run stylelint
+npm run hint
+npm run eslint
+```
+
+<!-- AUTHORS -->
+
+## 👥 Author <a name="authors"></a>
+
+> Mention all of the collaborators of this project.
+
+This project is solo
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p> top</a>)</p>
+
+## 🔭 Future Features <a name="future-features"></a>
+
+> Describe 1 - 3 features you will add to the project.
+
+- [ ] **[new_feature_1]**
+- [ ] **[new_feature_2]**
+- [ ] **[new_feature_3]**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTRIBUTING -->
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- SUPPORT -->
+
+## ⭐️ Show your support <a name="support"></a>
+
+If you like this project stars the repo.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+> Give credit to everyone who inspired your codebase.
+> I would like to thank...shayan my czn
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ index.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-vars */
+const topPlayersInfo = [
+  {
+    name: 'Jos Buttler',
+    country: 'Somerset, England',
+    description: 'Buttler, who was recently named the England white-ball captain.',
+  },
+  {
+    name: 'Babar Azam',
+    country: 'Lahore,Punjab Pakistan',
+    description: 'Babar azam has the highest scorer in World Cup 2021.',
+  },
+  {
+    name: 'Hardik Pandya',
+    country: 'Surat,Gujrat India',
+    description: 'After a difficult few years, Hardik Pandya is now back to his best.',
+  },
+  {
+    name: 'Wanindu Hasarng',
+    country: 'Galle, Sri Lanka',
+    description: 'he made his Sri Lankan debut in july 2017,playing for the team against pakistan.',
+  },
+];
+
+const playersName = document.querySelectorAll('.players-name');
+const playersCountry = document.querySelectorAll('.players-country');
+const playersDescription = document.querySelectorAll('.players-description');
+
+for (let i = 0; i < playersName.length; i++) {
+  playersName[i].innerHTML = topPlayersInfo[i].name;
+  playersCountry[i].innerHTML = topPlayersInfo[i].country;
+  playersDescription[i].innerHTML = topPlayersInfo[i].description;
+}
+
+const desktopMenuHome = document.querySelector('.menu-home-desktop');
+const desktopMenuAbout = document.querySelector('.menu-about-desktop');
+
+const homePageSection = document.querySelector('.home-page-section');
+const aboutSection = document.querySelector('.about-section');
+
+const mobileMenu = document.querySelector('.mobile-menu');
+
+document.querySelector('nav').addEventListener('click', (e) => {
+  if (e.target.classList[0] === 'navbar-lines-div-mobile' || e.target.classList[0] === 'lines') {
+    mobileMenu.classList.add('mobile-menu-appear');
+  }
+  if (e.target.classList[0] === 'menu-home-desktop') {
+    homePageSection.classList.remove('home-page-section-disappear');
+    aboutSection.classList.remove('about-section-apper');
+    desktopMenuHome.classList.remove('menu-home-desktop-appear');
+    desktopMenuAbout.classList.remove('menu-about-desktop-disappear');
+  }
+  if (e.target.classList[0] === 'menu-about-desktop') {
+    homePageSection.classList.add('home-page-section-disappear');
+    aboutSection.classList.add('about-section-apper');
+    desktopMenuHome.classList.add('menu-home-desktop-appear');
+    desktopMenuAbout.classList.add('menu-about-desktop-disappear');
+  }
+});
+
+const menuHome = document.querySelector('.menu-home');
+const menuAbout = document.querySelector('.menu-about');
+
+document.querySelector('.mobile-menu').addEventListener('click', (e) => {
+  if (e.target.classList[0] === 'mobile-menu-disappear-btn') {
+    mobileMenu.classList.remove('mobile-menu-appear');
+  }
+  if (e.target.classList[0] === 'menu-home') {
+    menuHome.classList.remove('menu-home-appear');
+    menuAbout.classList.remove('menu-about-disappear');
+    homePageSection.classList.remove('home-page-section-disappear');
+    aboutSection.classList.remove('about-section-apper');
+    mobileMenu.classList.remove('mobile-menu-appear');
+  }
+  if (e.target.classList[0] === 'menu-about') {
+    menuHome.classList.add('menu-home-appear');
+    menuAbout.classList.add('menu-about-disappear');
+    homePageSection.classList.add('home-page-section-disappear');
+    aboutSection.classList.add('about-section-apper');
+    mobileMenu.classList.remove('mobile-menu-appear');
+  }
+});
